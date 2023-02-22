cbf837d866697f91005a4a0cc618987b5688fdaf
Alan García <alan.ga.g281301@gmail.com>
2023-02-22 13:28:11 -0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# ecommerce
+# Client
+
+## Descripcion
+
+Antes se debe de posicionar en la carpeta de `client`.
+
+Se levanta el proyecto con
+
+```
+npm start
+```
+
+# Server
+
+## Descripcion
+
+Antes se debe de posicionar en la carpeta de `server`.
+
+Se levanta el proyecto con
+
+```
+npm start
+```
---
+++ server/src/index.js
@@ -0,0 +1,20 @@
+const { request, response } = require('express');
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.json())
+
+app.get('/',(request, response)=>{
+    response.json({
+        status:200,
+        message: "Hola"
+    })
+})
+
+
+app.listen(+port, () =>{
+    console.log(`Server listen por ${port}`)
+})
